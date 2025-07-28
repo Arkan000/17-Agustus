7cf0884604e04c325779ee406950a26ef346da05
Arkan000 <rasyaarkananta01@gmail.com>
2025-07-28 17:08:03 +0700

    Add files via upload

---
+++ 17 agus/script.js
@@ -0,0 +1,17 @@
+const nama  = document.getElementById('nama')
+const alamat  = document.getElementById('alamat')
+const form  = document.getElementById('form')
+
+form.addEventListener('submit', function(event){
+    var nonumber = /\d/.test(nama.value)
+    if(nonumber) {
+        alert('Nama tidak boleh mengandung angka');
+        event.preventDefault()
+    }else if ( nama.value == '' || nama.value == null){
+        alert('Mohon masukkan nama anda')
+        event.preventDefault()
+    }else if(alamat.value == '' || alamat.value == null){
+        alert('Mohon isi alamat anda dengan lengkap')
+        event.preventDefault()
+    }
+})
